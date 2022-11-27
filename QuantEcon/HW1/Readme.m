2654a5debf35a6597c4e599662199a759a3ea833
Carlos Rojas Quiroz <carlosrojasquiroz@gmail.com>
2022-11-28 00:13:51 +0100

    Update Readme.m

--- QuantEcon/HW1/Readme.m
+++ QuantEcon/HW1/Readme.m
@@ -1,1 +1,5 @@
-Readme
+This folder contains files for solving the Homework 1 of "Computations and Quantitative Models in Macro", in the PhD programme in Economics at the EUI. 
+
+If you want to replicate the graphs in the slides, please open first the file Master.m.
+
+# Master.m 
