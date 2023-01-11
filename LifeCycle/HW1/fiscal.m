04b4a092e09feb4dcf177d5e98eab3407a0d3a6d
Carlos Rojas Quiroz <carlosrojasquiroz@gmail.com>
2023-01-11 18:48:26 +0100

    Delete LifeCycle/HW1 directory

--- LifeCycle/HW1/fiscal.m
+++
@@ -1,6 +0,0 @@
-function [theta,b]=fiscal(p,m)
-    theta=p.omega*sum(m.Psi(p.JR:p.J))/sum(m.Psi(1:p.JR-1));
-    b_aux=theta*p.w*p.L./sum(m.Psi(p.JR:p.J));
-    b=zeros(p.J,1);
-    b(p.JR:p.J,1)=ones(p.J-p.JR+1,1).*b_aux;
-end
--- LifeCycle/HW1/grids.m
+++
@@ -1,7 +0,0 @@
-m.a_grid=linspace(p.a_min,p.a_max,p.naa);
-[m.logZ,m.Pz]=Tauchen(p.mu_z,p.rho_z,p.sigma_z,p.nzz,p.m_z);
-for ii=1:p.nzz
-    m.z_grid(ii)=exp(m.logZ(ii));
-end
-m.z_grid=(m.z_grid-mean(m.z_grid))/std(m.z_grid)+1;
-clear ii
